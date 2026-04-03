3d8ae7502add4758660e85253e8a644613c366bc
masud <mrmasud151821@gmail.com>
2026-04-03 12:02:09 +0600

    name typed

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -17,6 +17,8 @@ class TestController extends Controller
         return [
             $this->payment->pay('bkash',1000),
             $this->payment->pay('nagad', 2000)
+            
+            /* Masud rana */
         ];
     }
     
